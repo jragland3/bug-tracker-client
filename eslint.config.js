0d837ad36e9864cfc4577d8e8e95b8a359bbc5e1
Josh Ragland <59399189+jragland3@users.noreply.github.com>
2025-08-06 21:37:54 -0500

    Fixed errors

--- eslint.config.js
+++ eslint.config.js
@@ -47,31 +47,3 @@ export default [
     },
   },
 ];
-
-// export default tseslint.config(
-//   { ignores: ['dist'] },
-//   {
-//     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-//     files: ['**/*.{ts,tsx}'],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//     plugins: {
-//       'react-hooks': reactHooks,
-//       'react-refresh': reactRefresh,
-//     },
-//     rules: {
-//       ...reactHooks.configs.recommended.rules,
-//       'react-refresh/only-export-components': [
-//         'warn',
-//         { allowConstantExport: true },
-//       ],
-//       'quotes': ['error', 'single'],
-//       'semi': ['error', 'always'],
-//       'no-extra-semi': 'error',
-//       'space-before-function-paren': ['error', 'never'],
-//       'no-multi-spaces': 'error',
-//     },
-//   },
-// )
