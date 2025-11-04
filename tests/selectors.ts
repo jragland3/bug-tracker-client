b715d50aa63e5014f08c3882734eaa03fc4d2fe3
Josh Ragland <59399189+jragland3@users.noreply.github.com>
2025-11-04 10:11:31 -0600

    linting

--- tests/selectors.ts
+++ tests/selectors.ts
@@ -2,7 +2,7 @@ export const bugSelector = {
   description: '[data-test-id="bug-description"]',
   deleteButton: '[data-test-id="delete-button"]',
   title: '[data-test-id="bug-title"]'
-}
+};
 
 export const bugFormSelector = {
   descriptionField: '[data-test-id="description-field"]',
