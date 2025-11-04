f11d0bca19947c6dc61028292e58b1cbbef2e9d2
Josh Ragland <59399189+jragland3@users.noreply.github.com>
2025-11-04 10:20:14 -0600

    Abstracted selectors to new /tests/selectors.ts file (#16)
    * Abstracted selectors to new /tests/selectors.ts file
    
    * linting
    
    * Updated variable names

--- tests/e2e/reportBug.spec.ts
+++ tests/e2e/reportBug.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { selector } from '../selectors';
 
 
 test.describe('Report Bug Page', () => {
@@ -11,16 +12,16 @@ test.describe('Report Bug Page', () => {
     const bugDescription = `This bug was created by an E2E test. - ${Date.now()}`;
 
     // Verify seed bug displays
-    await expect(page.locator('[data-test-id="bug-title"]', { hasText: 'Seed Bug' })).toBeVisible();
+    await expect(page.locator(selector.bug.title, { hasText: 'Seed Bug' })).toBeVisible();
 
     // Fill out form
-    await page.fill('[data-test-id="title-field"]', bugTitle);
-    await page.fill('[data-test-id="description-field"]', bugDescription);
+    await page.fill(selector.bugForm.titleField, bugTitle);
+    await page.fill(selector.bugForm.descriptionField, bugDescription);
 
     // Submit form
-    await page.click('[data-test-id="submit-bug-button"]');
+    await page.click(selector.bugForm.submitButton);
 
     // Verify that the bug appears in the list
-    await expect(page.locator('[data-test-id="bug-title"]', { hasText: bugTitle })).toBeVisible();
+    await expect(page.locator(selector.bug.title, { hasText: bugTitle })).toBeVisible();
   });
 });
---
+++ tests/selectors.ts
@@ -0,0 +1,12 @@
+export const selector = {
+  bug: {
+    description: '[data-test-id="bug-description"]',
+    deleteButton: '[data-test-id="delete-button"]',
+    title: '[data-test-id="bug-title"]',
+  },
+  bugForm: {
+    descriptionField: '[data-test-id="description-field"]',
+    submitButton: '[data-test-id="submit-bug-button"]',
+    titleField: '[data-test-id="title-field"]',
+  },
+};
